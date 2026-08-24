cf35b8d57870e34f95bef40876dd6c4537437d26
yuvnex <palanivelyuvanesh@gmail.com>
2026-08-24 15:20:53 +0530

    fix:Implemented Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import RecursionVisualizer from './pages/RecursionVisualizer'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="*" element={<RecursionVisualizer />} />
       </Routes>
       <Toaster />
+      <Analytics />
     </Router>
   )
 }
